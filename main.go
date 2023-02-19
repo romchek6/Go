2047413ea39e47dd45455eddc7c387942608d403
veden <stud0000248921@study.utmn.ru>
2023-02-20 03:14:00 +0500

    First commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/lib/pq"
+	"html/template"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "index", nil)
+}
+
+func handleFunc() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func main() {
+
+	handleFunc()
+
+}
